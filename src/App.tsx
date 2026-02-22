94afb0ade0f457e6776b860653093db829047249
Ravi <rsingh4@sensata.com>
2026-02-22 11:00:22 +0000

    change

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 function App() {
   return (
     <div className="App">
+      <h1>Hello World</h1>
       <form>
         <input id="image" type="file" name="image" />
       </form>
