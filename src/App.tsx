e3c758ac824cbda71d80a13383daf529ed1ba199
Ravi <rsingh4@sensata.com>
2026-02-22 18:37:52 +0000

    Added Image upload component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css'
-
+import ImageUpload from "./components/ImageUpload";
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <form>
-        <input id="image" type="file" name="image" />
-      </form>
+      <ImageUpload />
     </div>
   )
 }
