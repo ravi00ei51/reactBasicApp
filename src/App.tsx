93b344861c270d5d6a7a99fd97bc80346fd529ab
Ravi <rsingh4@sensata.com>
2026-02-22 10:33:50 +0000

    First
    First

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <form>
+        <input id="image" type="file" name="image" />
+      </form>
+    </div>
+  )
+}
+
+export default App
